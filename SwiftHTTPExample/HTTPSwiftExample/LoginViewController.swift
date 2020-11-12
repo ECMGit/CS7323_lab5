c06c345787ffd0c0e4380bf8d28afb398efd145b
reidaruss <reidarussell@gmail.com>
2020-11-12 13:06:12 -0600

    Added Login/Register page and new view controller.

---
+++ SwiftHTTPExample/HTTPSwiftExample/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  HTTPSwiftExample
+//
+//  Created by Reid Russell on 11/12/20.
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ErrorLabel: UILabel!
+    @IBOutlet weak var UsernameBox: UITextField!
+    @IBOutlet weak var PasswordBox: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func RegisterButton(_ sender: UIButton) {
+    }
+    
+}
